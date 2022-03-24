0574e1f830a5448a57f2cf807c10073fdae5d66f
Jon Wexler <jwexman@gmail.com>
2022-03-24 10:31:46 -1000

    [main] added notes and intro chap

---
+++ chapter 4/4_1/index.js
@@ -0,0 +1,9 @@
+// Will teach about how an RSS feed works
+// 1. Effectively an API with one endpoint
+// What is an RSS Atom
+// Pull from external RSS examples:
+// Start with rss package? https://www.npmjs.com/package/rss
+// Data as an array of objects
+// https://www.devextent.com/xml-rss-feed-nodejs/
+// 2. Add your own endpoint for new content to mix in
+// 3. Add Mongo DB for saving top content
